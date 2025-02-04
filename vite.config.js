e6e31497239888583a0f1e5ba745d0fa56aff2d8
unknown <jackknightpayments@gmail.com>
2025-02-03 23:55:19 -0800

    Update vite.config.js and index.html for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/georadio/', // Add this line
+  base: '/guess-country/',
 })
